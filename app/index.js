e74a16decd1c5f412224251893c134e4676bae62
krypteo <kryptonicrevolution@gmail.com>
2023-07-01 19:33:29 +0300

    Hello there lol

--- app/index.js
+++ app/index.js
@@ -17,7 +17,7 @@ const Home = () => {
             <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
           ),
           headerRight: () => (
-            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="75%" />
           ),
           headerTitle: "",
         }}
